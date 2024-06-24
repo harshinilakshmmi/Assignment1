f5c40e20b6298c6e49a8ea04125af5ae2d541a8d
Harshini Lakshmmi <gharshini.lakshmi@hcl.com>
2024-06-25 00:32:28 +0530

    first commit

---
+++ UnitTest_MVVM/Manager/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  UnitTest_MVVM
+//
+//  Created by Harshini Lakshmmi on 24/06/24.
+//
+
+import Foundation
---
+++ UnitTest_MVVM/Model/TestDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestDataModel.swift
+//  UnitTest_MVVM
+//
+//  Created by Harshini Lakshmmi on 24/06/24.
+//
+
+import Foundation
